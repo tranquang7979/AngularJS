44aa4278da0c6020ced761a5151140f92c53ea3e
Steven Tran <tranquang7979@hotmail.com>
2019-08-07 14:02:19 +0700

    deploy website to the Firebase.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -8,10 +9,25 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent {
   items;
+  checkoutForm;
 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ) {
     this.items = this.cartService.getItems();
+
+    this.checkoutForm = this.formBuilder.group({
+      name: "",
+      address: ""
+    });
   }
+  // tslint:disable-next-line: typedef
+  onSubmit(customerData) {
+    console.warn("Your order has been submitted", customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
+
 }
